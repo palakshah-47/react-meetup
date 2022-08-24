364633ccd5094089a54a91361d97c1c560250ecd
palakshah-47 <palakshah47@gmail.com>
2022-08-24 12:30:54 -0400

    some changes

--- src/pages/NewMeetUp.js
+++ src/pages/NewMeetUp.js
@@ -1,6 +1,9 @@
-function newMeetUp() {
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
+function NewMeetUp() {
     return <section>
         <h1>Add New Meetup</h1>
+        <NewMeetupForm/>
     </section>
 }
-export default newMeetUp;
+export default NewMeetUp;
