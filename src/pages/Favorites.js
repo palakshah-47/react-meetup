5bca1e25dd33d7ed4bda8040ee8b2e0031a89775
palakshah-47 <107205778+palakshah-47@users.noreply.github.com>
2023-03-13 17:04:49 -0400

    adding context

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,4 +1,17 @@
-function favorites() {
-    return <div>Favorites page</div>
+import { useContext } from 'react';
+import FavoriteContext from '../store/favorite-context';
+import MeetupList from '../components/meeetups/MeetupList';
+
+function FavoritesPage() {
+    const favoritesContext = useContext(FavoritesContext);
+    return(
+        <section>
+          <h1>Favorites Page</h1>
+        {favoritesContext.totalFavorites === 0 ?
+        <p>You have no favorites yet. Start adding some!!</p>:
+        <MeetupList meetUps = {favoritesContext.favorites}/>}
+        </section>
+     )
+   
 }
-export default favorites;
+export default FavoritesPage;
