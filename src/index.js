27c95538f6dacaa7234fe3945e75efc1373d9b07
palakshah-47 <107205778+palakshah-47@users.noreply.github.com>
2023-03-13 16:55:33 -0400

    added context

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { FavoriteContextProvider } from './store/favorite-context';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<FavoriteContextProvider>
+                  <App />
+                </FavoriteContextProvider>, 
+                document.getElementById('root'));
   
